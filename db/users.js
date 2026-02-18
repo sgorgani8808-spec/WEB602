053593c23f0141471722cddd121a44db6493a6a9
sgorgani8808-spec <s.gorgani.8808@westcliff.edu>
2026-02-17 20:21:11 -0800

    Fix server setup and passport config

--- db/users.js
+++ db/users.js
@@ -1,18 +1,7 @@
 var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+  { id: 1, username: 'westcliff', password: 'secret', displayName: 'WestCliff', emails: [{ value: 'west@example.com' }] },
+  { id: 2, username: 'westcliffclass', password: 'birthday', displayName: 'WestCliff University', emails: [{ value: 'cliff@example.com' }] }
 ];
-
-exports.findById = function(id, cb) {
-  process.nextTick(function() {
-    var idx = id - 1;
-    if (records[idx]) {
-      cb(null, records[idx]);
-    } else {
-      cb(new Error('User ' + id + ' does not exist'));
-    }
-  });
-}
 
 exports.findByUsername = function(username, cb) {
   process.nextTick(function() {
@@ -24,4 +13,16 @@ exports.findByUsername = function(username, cb) {
     }
     return cb(null, null);
   });
-}
+};
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.id === id) {
+        return cb(null, record);
+      }
+    }
+    return cb(new Error('User ' + id + ' does not exist'));
+  });
+};
